eeabf5053e198ac92357562cfb1024a2488d0f95
unknown <41710688@Ad01.sec.com>
2020-07-16 11:45:58 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace test
+{
+    class BaseClass 
+    {
+        public BaseClass()
+        {
+            Console.WriteLine("BaseType Default Constructor");
+        }
+        public BaseClass(int i)
+        {
+            Console.WriteLine($"BaseType Non-Default Constructor:parameter 1,{i}");
+        }
+    }
+    class DerivedClass:BaseClass
+    {
+        public DerivedClass()
+        {
+            Console.WriteLine("DerivedClass Default Constructor");
+        }
+        public DerivedClass(int i,int j):base(i)
+        {
+            Console.WriteLine($"DerivedClass Non-Default Constructor:parameter 1,{i};parameter 2,{j}");
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            DerivedClass derivedClass = new DerivedClass(2, 3);
+            Console.ReadKey();
+        }
+    }
+}
